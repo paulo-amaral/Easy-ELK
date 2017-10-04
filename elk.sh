7dc3015acc821fc0f787afb16d43c352a0c95b6e
Paulo Sérgio Amaral <paulo.security@gmail.com>
2017-10-04 11:19:01 +0900

    Update elk.sh

--- elk.sh
+++ elk.sh
@@ -1,9 +1,11 @@
 
 #!/bin/bash
-#
-# This script is used to install ELK STACK
+#This script is used to install ELK STACK and mao database GeoLite2
 #Elasticsearch requires Java 8 or later. Use the official Oracle distribution or an open-source distribution such as OpenJDK.
-MYDOMAIN="mydomain"
+#Author : Paulo Amaral 
+
+
+MYDOMAIN="mp.tl"
 
 
 #Verify running as root:
@@ -31,7 +33,7 @@ check_apache2() {
     APACHE2=$(dpkg-query -W -f='${Status}' apache 2>/dev/null | grep -c "ok installed")
     if [ $APACHE2 -eq 0 ] ; then
         echo "Apache2 not installed - Installing Apache2 now - Please wait \n"
-        apt-get install apache2 apache2-doc apache2-utils
+        apt-get install -y apache2 apache2-doc apache2-utils
     else
         echo "Apache2 is installed "
     fi
@@ -84,7 +86,6 @@ configure_elasticsearch() {
     clear
     echo -n "Configuring elasticsearch \n"
     echo    "---------------------------"
-    
     cd /etc/elasticsearch/ || exit
     #bootstrap.memory_lock: true
     sed -i '/bootstrap.memory_lock:/s/^#//g' elasticsearch.yml
@@ -99,8 +100,9 @@ configure_elasticsearch() {
     #start service
     systemctl daemon-reload
     echo -n "Starting Service elasticsearch \n"
-    service elasticsearch start
-    sleep 3
+    echo    "------------------------------"
+    service elasticsearch start  
+    sleep 60
     #check if service is running
     SVC='elasticsearch'
     if ps ax | grep -v grep | grep $SVC > /dev/null ; then
@@ -117,42 +119,49 @@ install_kibana() {
     clear
     echo -n "Installing kibana \n"
     echo    "---------------------------"
-    cd /etc/kibana || exit
+    #get eth IP
+    IP=$(ip addr show |grep "inet " |grep -v 127.0.0. |head -1|cut -d" " -f6|cut -d/ -f1)
     #install package
     apt-get install -y kibana
-    #enable kibana
-    update-rc.d kibana defaults
+    cd /etc/kibana || exit
     #server.port: 5601
-    sed -i '/server.port:/s/^#//g' /etc/kibana/kibana.yml
-    # server.host: "localhost"
-    sed -i '/server.host:/s/^#//g' /etc/kibana/kibana.yml
-    #elasticsearch.url: "http://localhost:9200"
+    sed -i "/server.port:/s/^#//g" /etc/kibana/kibana.yml
+    #The default is 'localhost', which usually means remote machines will not be able to connect.
+    #server.host: "localhost"
+    sed -i "/server.host/aserver.host: ${IP}"  /etc/kibana/kibana.yml
+    #Elastic url
     sed -i '/elasticsearch.url:/s/^#//g' /etc/kibana/kibana.yml
     #start kibana
-    update-rc.d kibana defaults 96 9
-    service kibana start
-    sleep 3
+    echo -n "Updating start daemon Kibana \n"
+    echo    "---------------------------"
+    CMD=$(command -v systemctl)
+    if [ $CMD > /dev/null ] ; then
+        systemctl daemon-reload
+        systemctl enable kibana.service
+        systemctl start kibana.service
+    else
+        update-rc.d kibana defaults 95 10
+        service kibana start
+    fi
     #enable Kibana mod proxy
     a2enmod proxy
     a2enmod proxy_http
-    #check if port is active
-    KBSVC='kibana'
-    if ps ax | grep -v grep | grep $KBSVC > /dev/null ; then
-        echo "Kibana service is running"
-    else
-        echo "Kibana Server is stopped - please check your installation"
-    fi
-    exit 1
+    a2enmod rewrite && /etc/init.d/apache2 restart
+        
 }
 
 
 #Create apache config file for kibana
 #please edit ServerName and ServerAdmin
 configure_kibana() {
+    clear
+    echo -n "Configuring Kibana \n"
+    echo    "---------------------------"
     cd /etc/apache2/sites-available || exit
     #create file
     touch  kibana.conf
     #insert config
+    APACHE_LOG_DIR='/var/log/apache2'
 cat <<- EOF > kibana.conf
   <VirtualHost *:80>
   ServerName kibana.$MYDOMAIN
@@ -173,6 +182,16 @@ EOF
     #enable apache config file
     a2ensite kibana.conf
     service apache2 reload
+
+    #check if KIBANA port is active
+    KBSVC='kibana'
+    if ps ax | grep -v grep | grep $KBSVC > /dev/null ; then
+        echo "Kibana service is running \n"
+    else
+        echo "Kibana Server is stopped - please check your installation"
+        exit 1  
+    fi
+     service apache2 reload
 }
 
 #Install and Configure Logstash
@@ -183,11 +202,31 @@ install_logstash() {
     touch /etc/logstash/conf.d/logstash.conf
     cd /etc/logstash/conf.d/ || exit
     #start logstash
-    initctl start logstash
+    systemctl daemon-reload
+    systemctl start logstash.service
+    systemctl enable logstash.service
     #install geolocation data for maps
     cd /etc/logstash || exit
     curl -O "http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz"
     gunzip GeoLite2-City.mmdb.gz
+}
+
+test_elasticsearch_port(){
+clear
+    echo -n "Testing if Elasticsearch is Ruuning on port 9200 \n"
+    echo    "---------------------------------------------------"   
+PORT=9200
+URL="http://localhost:$PORT"
+# Check that Elasticsearch is running
+curl -s $URL 2>&1 > /dev/null
+if [ $? != 0 ]; then
+    echo "Unable to contact Elasticsearch on port $PORT."
+    echo "Please ensure Elasticsearch is running and can be reached at $URL"
+    exit -1
+    else
+    echo -n "Service is Running \n"
+    
+fi
 }
 
 check_user
@@ -195,7 +234,8 @@ update_system_packages
 check_apache2
 check_java
 install_elasticsearch
-configure_elasticsearch
+configure_elasticsearch #parei aqui
 install_kibana
 configure_kibana
 install_logstash
+test_elasticsearch_port
